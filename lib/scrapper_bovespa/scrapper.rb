c0bea75011a8fd93c11dac7a205f872886705c2e
Ubuntu <ubuntu@ip-172-31-28-0.us-west-2.compute.internal>
2018-06-06 00:06:58 +0000

    Extracts heading fields.

--- lib/scrapper_bovespa/scrapper.rb
+++ lib/scrapper_bovespa/scrapper.rb
@@ -38,8 +38,71 @@ class ScrapeCompanys
 		@code_source = "http://cvmweb.cvm.gov.br/SWB/Sistemas/SCW/CPublica/CiaAb/FormBuscaCiaAbOrdAlf.aspx?LetraInicial="
 	end
 
+	def extract_simple_text(source)
+		simplified = source[0].reverse
+		simplified = simplified[(simplified.index(">dt/<") + 5)..(simplified.index(">dt<") - 1)]
+		simplified.reverse
+	end
+
+	def extract_site(source)
+		simplified = source[0].reverse
+		simplified = simplified[(simplified.index(">a/<") + 4)..(simplified.index(">\"") - 1)]
+		simplified.reverse
+	end
+
+	def extract_par_th(source)
+		puts source
+	end
+
 	def get_company_data(cvm)
-		source - Net::HTTP.get(URI("#{@base_query_address}#{cvm}"))
+		output = {}
+		source = Net::HTTP.get(URI("#{@base_query_address}#{cvm}"))
+		output[:nome] = extract_simple_text(source.scan(/Nome de Preg.+\r\n.+\<\/td\>/))
+		output[:cnpj] = extract_simple_text(source.scan(/CNPJ.+\r\n.+\<\/td\>/))
+		output[:site] = extract_site(source.scan(/Site:\<\/td\>\r\n.+\<\/a\>/))
+
+		cabecalho = extract_par_th(source.scan(/Balan.o Patrimonial.+\r\n.+\r\n.+\<\/th\>/))
+=begin
+		ativo_permanente = source.scan(/Ativo Permanente\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+		ativo_total = source.scan(/Ativo Total\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+		patrimonio_liquido = source.scan(/Patrimônio Líquido\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+
+		#separa os dados em a e b
+		output[:balanco_patrimonial] = {ativo_permanente: [ativo_p_a, ativo_p_b],
+																		ativo_total: [ativo_t_a, ativo_t_b],
+																		patrimonio_liquido: [patrimonio_a, patrimonio_b],
+																		periodo: [data_a, data_b]}
+
+		cabecalho_demonstracao_resultado = source.scan(/Demonstração do Resultado - Não-Consolidado\:\<\/th\>\<th\>.+\<\/th\>\<th\>.+\<\/th\>/)
+		receitas = source.scan(/Receitas da Intermediação Financeira\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+		resultado_bruto = source.scan(/Resultado Bruto da Intermediação Financeira\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+		resultado_operacional = source.scan(/Resultado Operacional\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+		lucro = source.scan(/Lucro (Prejuízo) Líquido\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+
+		#separa os dados em a e b
+		output[:demonstracao_do_resultado] = {receitas_da_intermediacao_financeira: [receitas_i_a, receitas_i_b],
+																					resultado_bruto_da_intermediacao_financeira: [resultado_b_a, resultado_b_b],
+																					resultado_operacional: [resultado_a, resultado_b],
+																					lucro_liquido: [lucro_a, lucro_b],
+																					periodo: [periodo_a, periodo_b]}
+
+		
+		cabecalho_fluxo_caixa = source.scan(/Demonstração do Fluxo de Caixa - Não-Consolidado\:\<\/th\>\<th\>.+\<\/th\>\<th\>.+\<\/th\>/)
+		operacionais = source.scan(/Atividades Operacionais\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+		investimento = source.scan(/Atividades de Investimento\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+		financiamento = source.scan(/Atividades de Financiamento\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+		variacao = source.scan(/Variação Cambial sobre Caixa e Equivalentes\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+		aumento = source.scan(/Aumento (Redução) de Caixa e Equivalentes\:\<\/td\>\<td\>.+\<\/td\>\<td\>.+\<\/td\>/)
+
+		#separa os dados em a e b
+		output[:demonstracao_do_fluxo_de_caixa] = {atividades_operacionais: [operacionais_a, operacionais_b],
+		atividades_de_investimento: [investimento_a, investimento_b],
+		atividades_de_financiamento: [financiamento_a, financiamento_b],
+		variacao_cambial_sobre_caixa_e_equivalentes: [variacao_a, variacao_b],
+		aumento_de_caixa_e_equivalentes: [aumento_a, aumento_b],
+		periodo: [periodo_a, periodo_b]}	
+=end
+		output
 	end
 
 	def get_cvms()
--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Scrapper do 
+=begin
 	it 'can extract an array of integer codes' do
 		scrap = ScrapeCompanys.new()
 		cvms = scrap.get_cvms()
@@ -8,11 +9,23 @@ RSpec.describe Scrapper do
 		expect(cvms.class.to_s.downcase).to eq('array')
 		expect(cvms.size > 0).to eq(true)
 	end
-
+=end
 	it 'can extract a complete company given a valid cvm' do
 		scrap = ScrapeCompanys.new()
 		data = scrap.get_company_data(1821)
 
-		expect('something').to eq('other thing')
+		expect(data[:nome]).to eq('ITAUBANK LS')
+		expect(data[:cnpj]).to eq('43.443.464/0001-85')
+		expect(data[:site]).to eq('www.itau.com.br')
+		expect(data[:balanco_patrimonial].class.to_s).to eq('Hash')
+		expect(data[:balanco_patrimonial][:ativo_permanente].class.to_s).to eq('Array')
+		expect(data[:demonstracao_do_resultado].class.to_s).to eq('Hash')
+		expect(data[:demonstracao_do_resultado][:resultado_operacional].class.to_s).to eq('Array')
+		expect(data[:demonstracao_fluxo_caixa].class.to_s).to eq('Hash')
+		expect(data[:demonstracao_fluxo_caixa][:aumento_caixa_equivalentes].class.to_s).to eq('Array')
+		expect(data[:posicao_acionaria].class.to_s).to eq('Hash')
+		expect(data[:posicao_acionaria][:total].class.to_s).to eq('Array')
+		expect(data[:composicao_capital_social].class.to_s).to eq('Hash')
+		expect(data[:composicao_capital_social][:total].class.to_s).to eq('String')
 	end
 end
